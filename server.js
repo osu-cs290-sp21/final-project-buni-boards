d3bd60d7d8d131f23ad1dfdc24283389078cc1e6
Parker Conrad <parkerconrad814@yahoo.com>
2021-06-07 08:09:19 -0700

    buni cleanup

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 
+//Import the mongoose module
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 var app = express();
 var port = process.env.PORT || 3000;
 
